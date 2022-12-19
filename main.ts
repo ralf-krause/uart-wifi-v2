dc687fae71c11843a0360e518bbbfdf18f6238c7
Ralf Krause <ralf.krause@gmail.com>
2022-12-19 20:04:14 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,12 @@ BaudRate.BaudRate115200,
 "Bertha",
 "12345678"
 )
+if (communication.wifiOK()) {
+    basic.showIcon(IconNames.Yes)
+    music.playTone(262, music.beat(BeatFraction.Whole))
+} else {
+    basic.showIcon(IconNames.No)
+}
 basic.forever(function () {
 	
 })
