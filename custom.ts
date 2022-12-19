1118dabcaef3dda261dd26fc0476d27922f207ec
Ralf Krause <ralf.krause@gmail.com>
2022-12-19 19:48:21 +0100

    Aktualisiere main.blocks, main.ts, custom.ts

--- custom.ts
+++ custom.ts
@@ -15,10 +15,10 @@ namespace communication {
     /**
      * Setup Grove - Uart WiFi V2 to connect to  Wi-Fi
      */
-    //% block="Setup Wifi|TX %txPin|RX %rxPin|Baud rate %baudrate|SSID = %ssid|Password = %passwd"
+    //% block="setup Wifi TX %txPin RX %rxPin Baud rate %baudrate|SSID = %ssid Password = %passwd"
     //% group="UartWiFi"
-    //% txPin.defl=SerialPin.P15
-    //% rxPin.defl=SerialPin.P1
+    //% txPin.defl=SerialPin.C17
+    //% rxPin.defl=SerialPin.C16
     //% baudRate.defl=BaudRate.BaudRate115200
     export function setupWifi(txPin: SerialPin, rxPin: SerialPin, baudRate: BaudRate, ssid: string, passwd: string) {
         let result = 0
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+communication.setupWifi(
+SerialPin.P0,
+SerialPin.P1,
+BaudRate.BaudRate115200,
+"",
+""
+)
 basic.forever(function () {
 	
 })
